0b1a011233e5f3d729f020da83584fa1036e9198
hennah.Lee <34415068+hyesue-lee@users.noreply.github.com>
2022-12-14 23:17:26 +0900

    Time: 1120 ms (50.38%), Space: 0B (100.00%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -4,8 +4,8 @@ SELECT distinct num ConsecutiveNums
 FROM(
     SELECT 
          num
-        ,LEAD(num) OVER( ORDER BY id) AS lag1
-        ,LEAD(num,2) OVER( ORDER BY id) AS lag2
+        ,LAG(num) OVER( ORDER BY id) AS lag1
+        ,LAG(num,2) OVER( ORDER BY id) AS lag2
         FROM logs
 )sub
 WHERE sub.num = sub.lag1 AND sub.lag1 = sub.lag2
