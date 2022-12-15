488175fbdcbb5eb8832adc0d9e3844aad46acf72
hennah.Lee <34415068+hyesue-lee@users.noreply.github.com>
2022-12-15 13:57:23 +0900

    Time: 731 ms (96.68%), Space: 0B (100.00%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,19 +1,18 @@
 /* 가장높은 급여인 직원을 찾아라. 각 부서 별로  */
-
+/*RANK 사용하기*/
 
 SELECT department
     ,employee
-    ,high_salary salary
+    ,salary
 FROM(
     SELECT D.name department
         , E.name employee
         , E.salary salary 
-        , MAX(E.salary) OVER(PARTITION BY D.id) high_salary
+        , DENSE_RANK() OVER(PARTITION BY D.id ORDER BY E.salary DESC) salary_rank
 
     FROM employee E 
         JOIN department D
         ON E.departmentId = D.id 
+    
  )sub
- WHERE sub.salary = sub.high_salary
- 
- 
+ WHERE sub.salary_rank = 1
