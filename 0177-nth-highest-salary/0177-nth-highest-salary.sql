516b46a63c2ce90d48a52eda9b43b407a9cf444a
hennah.Lee <34415068+hyesue-lee@users.noreply.github.com>
2022-12-21 23:11:39 +0900

    Time: 892 ms (7.58%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -2,13 +2,14 @@
     RETURNS INT
     BEGIN
     RETURN (
-            SELECT IF(COUNT(sub.salary) >= N, MIN(salary), null)
-            FROM (
-                SELECT DISTINCT salary
-                FROM employee
-                ORDER BY salary desc 
-                limit N 
-            )sub 
+            SELECT DISTINCT salary
+            FROM 
+            (
+				SELECT salary,
+				DENSE_RANK() OVER(ORDER BY SALARY DESC) AS drank  
+				FROM employee 
+            ) sub 
+            WHERE sub.drank = N
     );
     END
 
