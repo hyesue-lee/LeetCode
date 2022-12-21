1bb90c5705b47f3e5f0451b80585397eb557baaa
hennah.Lee <34415068+hyesue-lee@users.noreply.github.com>
2022-12-21 23:17:53 +0900

    Time: 459 ms (45.01%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -2,11 +2,11 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN 
   (
-            SELECT DISTINCT salary -- 존재하지않으면 어차피 null이라 별도 처리 안해줘도 됨.
+            SELECT salary -- 존재하지않으면 어차피 null이라 별도 처리 안해줘도 됨.
      
             FROM 
             (
-				SELECT salary
+				SELECT DISTINCT salary
 				,DENSE_RANK() OVER(ORDER BY SALARY DESC) AS drank
 				FROM employee 
             )sub 
