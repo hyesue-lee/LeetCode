e1029984f20ef89ebf63fb9f0b27ff604b61804c
hennah.Lee <34415068+hyesue-lee@users.noreply.github.com>
2023-01-18 02:40:24 +0900

    Time: 1461 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -2,12 +2,10 @@
 /*캐피탈 이득/손실
 stock 은 한번 또는 많이 산것과 판것 이후의 전체 이득, 손실 */
 
+
 SELECT stock_name
-,SUM(price) capital_gain_loss 
-FROM(
-    SELECT stock_name
-    ,operation
-    ,IF(operation = 'buy' , -price, price ) AS price 
-    FROM Stocks
-)sub 
+, SUM(IF(operation = 'buy' , -price, price )) AS capital_gain_loss 
+FROM Stocks
 GROUP BY stock_name
+
+
